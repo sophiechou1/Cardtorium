58ecf573650d071e821be3d0e85bc61f706f48af
j-wielg <jwielgus@nd.edu>
2024-04-10 08:26:36 -0400

    feat: Made a city sprite

--- Classes/city.gd
+++ Classes/city.gd
@@ -7,6 +7,6 @@ class_name City
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	centered = false
-	texture = load("res://Assets/Troop Sprites/default.png")
+	texture = load("res://Assets/Tiles/city.png")
 
 
